7dd4ea3e114e4a3e9c79f2d9891ae35c33c355d9
PravinKShepHertz <pravin.kumar@shephertz.co.in>
2014-06-30 17:20:13 +0530

    added logger

--- main.go
+++ main.go
@@ -5,9 +5,30 @@ import(
 	"net/http"
 	"text/template"
 	"log"
+	"io"
+  "io/ioutil"
+  "os"
 )
 
+var (
+    MyFile  *log.Logger
+)
+
+func Init(
+    traceHandle io.Writer,
+    infoHandle io.Writer,
+    warningHandle io.Writer,
+    errorHandle io.Writer) {
+
+    file, _ := os.OpenFile("log/production.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+    
+    MyFile = log.New(file,
+        "PREFIX: ",
+        log.Ldate|log.Ltime|log.Lshortfile)    
+}
+
 func rootHandler(w http.ResponseWriter, r *http.Request){
+  MyFile.Println("Inside Root Handler")
 	fmt.Println("Inside Root Handler")
 	t := template.New("index.html")
 	t.ParseFiles("public/index.html")
@@ -17,6 +38,8 @@ func rootHandler(w http.ResponseWriter, r *http.Request){
 }
 
 func main() {
+  Init(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
+	MyFile.Println("Inside Main Function")
 	http.HandleFunc("/", rootHandler)
 	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
 	if err := http.ListenAndServe("0.0.0.0:3000", nil); err != nil {
